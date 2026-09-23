ec95e041a43a457b7db00eb7ff4bcc90e0124a4f
midward <midwardisaac@gmail.com>
2026-09-23 10:05:12 -0500

    build fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Apunta '@' directamente a la carpeta 'd' donde tienes components, lib, pages, etc.
-      '@': path.resolve(__dirname, './dir'),
+      '@': fileURLToPath(new URL('./dir', import.meta.url)),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
